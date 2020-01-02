364c33cc1c3521df685bd559af62af266b93f5f5
Axel <axosar@gmail.com>
2020-01-02 17:23:39 -0600

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  let [counter, setCounter] = useState(0)
+  let [counter, setCounter] = useState(0);
+  const [data, setData] = useState({ hits: [] });
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios(
+        'https://hn.algolia.com/api/v1/search?query=redux',
+      );
+      setData(response.data);
+    }
+    fetchData();
+  }, []);
   const elano = (valor) => {
     console.log(valor);
+    
   }
   return (
     <div className="App">
       <header className="App-header">
         <h1>BIBLIOTECA DEL ROCK</h1>
         <h2>Historias De La Música Latinoamericana</h2>
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
   <p onClick={() => setCounter(counter + 1)}>Desarrollo del Rock a través de la Historia en Latinoamérica {counter}</p>
         <ul>
           <li><p onClick={() => elano(1950)}>1950</p></li>
