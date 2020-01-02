505b7c4f65d9a2ecf16053193758f2b87d56426e
Axel <axosar@gmail.com>
2020-01-02 11:36:03 -0600

    Updates

---
+++ precache-manifest.7c6784c85d1953008e93091e7f7e4ee4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc5e6fcd1fe8dd9d39fc4a8ab348ce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04cbb410efd68767ae4f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "21899f6c803794508238",
+    "url": "/static/js/2.1ad38bdb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1ad38bdb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04cbb410efd68767ae4f",
+    "url": "/static/js/main.e8846ff0.chunk.js"
+  },
+  {
+    "revision": "7a520a3db01d6acef800",
+    "url": "/static/js/runtime-main.5622fafc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
