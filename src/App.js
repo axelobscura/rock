dd4d27be02ad3638eb700a0b9888d0f29573d61a
Axel <axosar@gmail.com>
2020-01-02 15:54:11 -0600

    etting state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  let [counter, setCounter] = useState(0)
+  const elano = (valor) => {
+    console.log(valor);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>BIBLIOTECA DEL ROCK {counter}</h1>
+        <h2>Historias De La Música Latinoamericana</h2>
+        <p>Desarrollo del Rock a través de la Historia en Latinoamérica</p>
+        <ul>
+          <li><p onClick={() => elano(1950)}>1950</p></li>
+          <li><p onClick={() => elano(1960)}>1960</p></li>
+          <li><p onClick={() => elano(1970)}>1970</p></li>
+          <li><p onClick={() => elano(1980)}>1980</p></li>
+          <li><p onClick={() => elano(1990)}>1990</p></li>
+          <li><p onClick={() => elano(2000)}>2000</p></li>
+          <li><p onClick={() => elano(2010)}>2010</p></li>
+          <li><p onClick={() => elano(2020)}>2020</p></li>
+        </ul>
       </header>
     </div>
   );
